27a9752ddb6f6a467dd0de6a3f6d8097cae2ad8d
katherineorozco <150992954+katherineorozco@users.noreply.github.com>
2023-11-27 20:59:28 -0600

    added student_scores file
    gets the student scores and the best score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from grades import calculate_grade
 
+
 def get_score(num_students):
     while True:
         scores = input(f'Enter {num_students} score(s): ').split()
@@ -12,6 +13,7 @@ def get_score(num_students):
 
     return student_scores, best
 
+
 def show_result(student_scores, best):
     counter = 1
     for score in student_scores:
@@ -19,12 +21,25 @@ def show_result(student_scores, best):
         print(f'Student {counter} score is {score} and grade is {grade}')
         counter += 1
 
+
+def save_to_file(filename, student_scores, best):
+    with open(filename, 'w') as file:
+        file.write(f'Best Score: {best}\n')
+        file.write('Student Scores: ')
+        for score in student_scores:
+            file.write(f'{score}')
+            file.write(', ')
+
+
 def main():
+    filename = 'student_scores.py'
+
     num_students = int(input('Total number of students: '))
     student_scores, best = get_score(num_students)
     show_result(student_scores, best)
+    save_to_file(filename, student_scores, best)
+
 
 
 if __name__ == '__main__':
     main()
-
---
+++ student_scores.py
@@ -0,0 +1,2 @@
+Best Score: 38
+Student Scores: 20, 38, 
